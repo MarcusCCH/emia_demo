eddf1e917b02c05dd0f16cbd547317479e688795
MarcusCCH <marcuscch12@gmail.com>
2023-05-07 20:22:35 +0800

    fix css

--- client/src/LandingPage.js
+++ client/src/LandingPage.js
@@ -1,32 +1,37 @@
 import { useState, useLayoutEffect } from "react";
-import './LandingPage.css';
-
+import "./LandingPage.css";
 const LandingPage = (props) => {
-    const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false);
 
-    useLayoutEffect(() => {
-        setShow(true);
-    }, []);
+  useLayoutEffect(() => {
+    setShow(true);
+  }, []);
 
-    // const onAnimationEnd = () => {
-    //     console.log("onAnimationEnd");
-    //     setShow(false);
-    //     props.onAnimationComplete();
-    // }
-    const onAnimationComplete = () => {
-        setShow(false);
-        props.onAnimationComplete();
-    };
+  // const onAnimationEnd = () => {
+  //     console.log("onAnimationEnd");
+  //     setShow(false);
+  //     props.onAnimationComplete();
+  // }
+  const onAnimationComplete = () => {
+    setShow(false);
+    props.onAnimationComplete();
+  };
 
-    return (
-        <div className="container">
-            {show && (
-                <div className="box" onAnimationEnd={onAnimationComplete}>
-                    <div className="text">USTudyPet</div>
-                </div>
-            )}
+  return (
+    <div className="landing-container">
+      {" "}
+      {show && (
+        <div className="box" onAnimationEnd={onAnimationComplete}>
+          <img
+            src="/fire_bird_old.png"
+            alt="fire_bird"
+            style={{ width: "30px", height: "30px" }}
+          />{" "}
+          <div className="text">USTudyPet </div>{" "}
         </div>
-    );
+      )}{" "}
+    </div>
+  );
 };
 
 export default LandingPage;
